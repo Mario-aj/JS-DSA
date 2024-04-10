6dfd4729dc816c3aa8947e956ef4d097b79cc78a
mario-aj <mariojorge1997@mail.ru>
2024-04-10 16:32:26 +0100

    recursion: reversing the string

---
+++ recursion/reverseString.js
@@ -0,0 +1,14 @@
+/**
+ * reverse
+ * Write a recursive function called reverse which accepts a string and
+ * returns a new string in reverse.
+ */
+
+function reverse(str) {
+  if (!str) return "";
+
+  return str[str.length - 1] + reverse(str.substring(0, str.length - 1));
+}
+
+console.log(reverse("awesome") === "emosewa");
+console.log(reverse("rithmschool") === "loohcsmhtir");
