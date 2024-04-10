db2c16712fe07f44287a2f47fdfc4f2ccf97b6e5
mario-aj <mariojorge1997@mail.ru>
2024-04-10 20:59:01 +0100

    recursion: unsolved flatten exercise

---
+++ recursion/flatten.js
@@ -0,0 +1,17 @@
+/**
+ * flatten
+ * Write a recursive function called flatten which accepts an array of arrays and returns a
+ * new array with all values flattened.
+ */
+
+/**
+ * [1,2,3 [4, 5]]
+ *  1,
+ */
+
+function flatten(input) {}
+
+console.log(flatten([1, 2, 3, [4, 5]])); // [1, 2, 3, 4, 5]
+// console.log(flatten([1, [2, [3, 4], [[5]]]])); // [1, 2, 3, 4, 5]
+// console.log(flatten([[1], [2], [3]])); // [1,2,3]
+// console.log(flatten([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]])); // [1,2,3]
