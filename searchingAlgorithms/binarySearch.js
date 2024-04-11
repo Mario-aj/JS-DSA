0316509a691ef2ff0fdf02ceae4fa402ca846f40
mario-aj <mariojorge1997@mail.ru>
2024-04-11 13:16:26 +0100

    search: binary search (wip)

---
+++ searchingAlgorithms/binarySearch.js
@@ -0,0 +1,54 @@
+function binarySearch(arr, value) {
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left < right) {
+    let middle = Math.floor((left + right) / 2);
+
+    let current = arr[middle];
+
+    if (current === value) {
+      return middle;
+    }
+
+    if (current < value) {
+      left = ++middle;
+    } else if (current > value) {
+      right = --middle;
+    }
+  }
+
+  return -1;
+}
+
+console.log(binarySearch([1, 2, 3, 4, 5], 2)); // 1
+console.log(binarySearch([1, 2, 3, 4, 5], 3)); // 2
+console.log(binarySearch([1, 2, 3, 4, 5], 5)); // 4
+console.log(binarySearch([1, 2, 3, 4, 5], 6)); // -1
+console.log(
+  binarySearch(
+    [
+      5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 40, 44, 64, 79, 84, 86, 95, 96, 98,
+      99,
+    ],
+    10
+  )
+); // 2
+console.log(
+  binarySearch(
+    [
+      5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 40, 44, 64, 79, 84, 86, 95, 96, 98,
+      99,
+    ],
+    95
+  )
+); // 16
+console.log(
+  binarySearch(
+    [
+      5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 40, 44, 64, 79, 84, 86, 95, 96, 98,
+      99,
+    ],
+    100
+  )
+); // -1
