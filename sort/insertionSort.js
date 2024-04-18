96877b81cecb6d3b959aa10a131e788d44b91e85
mario-aj <mariojorge1997@mail.ru>
2024-04-18 09:57:26 +0100

    sort: insertionSort, write the idea and pseudocode behind the insertionSort algo

--- sort/insertionSort.js
+++ sort/insertionSort.js
@@ -1,3 +1,22 @@
+/**
+ * Idea
+ * Sort the array by picking  one element at a time and comper it with all
+ * elements before, make a swap of j+1 and j until find an element which is small then
+ *  the current one, and insert the current into j+1.
+ *
+ *
+ * PSEUDOCODE
+ *
+ * Starting loop over the entire array, by starting from the second position (index = 1).
+ * asign the currentValue as arra[i];
+ * make the inner loop which start from the i-1, and goes until j >= 0 and arr[j] > currentValue;
+ *  make a swap, or arr[j+1] = arr[j]
+ * After the inner loop, probabily j will be -1 or arr[j] will be less then currentValue.
+ *    So, insert the currentValue in j, position.
+ *      We should insert in j+1, because if j = -1, there is no position -1, or the arrj[j]
+ *        is less then currentValue.
+ */
+
 function insertionSort(arr = []) {
   for (let i = 1; i < arr.length; i++) {
     let current = arr[i];
