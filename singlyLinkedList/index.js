4ea2ff25242318544e3f19ac558f415cd0af46a9
mario-aj <mariojorge1997@mail.ru>
2024-04-28 12:21:27 +0100

    singlyLinkedList: pop function

--- singlyLinkedList/index.js
+++ singlyLinkedList/index.js
@@ -23,6 +23,26 @@ class SinglyLinkedList {
 
     this.tail = node;
     this.length++;
+
+    return this;
+  }
+
+  pop() {
+    if (!this.length) return undefined;
+
+    let currentNode = this.head;
+    let previousNode = null;
+
+    while (currentNode !== this.tail) {
+      previousNode = currentNode;
+      currentNode = currentNode.next;
+    }
+
+    previousNode.next = null;
+    this.tail = previousNode;
+    this.length--;
+
+    return currentNode;
   }
 }
 
@@ -33,4 +53,7 @@ list.push("hey");
 list.push("There");
 list.push("Mario");
 
+console.log(list);
+
+list.pop();
 console.log(list);
