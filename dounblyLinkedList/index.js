4a1ac3ccf6033de163daa86f5f8aa6331cf0a70e
mario-aj <mariojorge1997@mail.ru>
2024-05-03 13:44:06 +0100

    DLL: push and pop implementation

--- dounblyLinkedList/index.js
+++ dounblyLinkedList/index.js
@@ -12,4 +12,58 @@ class DoublyLinkedList {
     this.tail = null;
     this.length = 0;
   }
+
+  creatNode(value) {
+    return new Node(value);
+  }
+  push(value) {
+    const newNode = this.creatNode(value);
+
+    if (!this.head) {
+      this.head = newNode;
+    }
+
+    if (this.tail) {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+    }
+
+    this.tail = newNode;
+    this.length++;
+
+    return this;
+  }
+
+  pop() {
+    if (this.length === 0) return undefined;
+
+    const removedNode = this.tail;
+
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = removedNode.prev;
+      this.tail.next = null;
+      removedNode.prev = null;
+    }
+
+    this.length--;
+    return removedNode;
+  }
 }
+
+const list = new DoublyLinkedList();
+
+list.push(1);
+list.push(2);
+list.push(3);
+list.push(4);
+
+console.log(list);
+
+console.log(
+  "======================================== POP ========================================"
+);
+list.pop();
+console.log(list);
