d3eaa92fd1d75a8adbac98c32141acfff623a288
mario-aj <mariojorge1997@mail.ru>
2024-05-16 12:53:10 +0100

    Binary Heap: insert method

---
+++ binaryHeap/index.js
@@ -0,0 +1,38 @@
+class BinaryHeap {
+  constructor() {
+    this.values = [];
+  }
+
+  insert(value) {
+    this.values.push(value);
+
+    function bubble(values, index) {
+      if (index === 0) return values;
+
+      const parentIndex = Math.floor((index - 1) / 2);
+
+      if (values[index] < values[parentIndex]) return values;
+
+      let temp = values[index];
+      values[index] = values[parentIndex];
+      values[parentIndex] = temp;
+
+      return bubble(values, parentIndex);
+    }
+
+    this.values = bubble(this.values, this.values.length - 1);
+
+    return this;
+  }
+}
+
+const binaryHeap = new BinaryHeap();
+
+binaryHeap.insert(12);
+binaryHeap.insert(1);
+binaryHeap.insert(5);
+binaryHeap.insert(18);
+binaryHeap.insert(14);
+binaryHeap.insert(8);
+
+console.log(binaryHeap.values);
