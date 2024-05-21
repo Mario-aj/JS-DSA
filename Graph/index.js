f71a7169de0e9a10f32aceaec1af67ae729a34f0
mario-aj <mariojorge1997@mail.ru>
2024-05-21 20:50:54 +0100

    Graph: addVertex method

--- Graph/index.js
+++ Graph/index.js
@@ -2,8 +2,24 @@ class Graph {
   constructor() {
     this.adjacencyList = {};
   }
+
+  addVertex(node) {
+    if (!this.adjacencyList[node]) {
+      this.adjacencyList[node] = [];
+    }
+
+    return this;
+  }
 }
 
 const g = new Graph();
+
+g.addVertex("Luanda");
+g.addVertex("Benguela");
+g.addVertex("Bengo");
+g.addVertex("Uige");
+g.addVertex("Zaire");
+g.addVertex("Huambo");
+g.addVertex("Cunene");
 
 console.log(g.adjacencyList);
