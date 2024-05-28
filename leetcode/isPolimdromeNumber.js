c37edb795b857b6bd2104d6ac7b4798f81028707
mario-aj <mariojorge1997@mail.ru>
2024-05-28 19:53:09 +0100

    leetcode: reducing the time complexity of the algorithm

--- leetcode/isPolimdromeNumber.js
+++ leetcode/isPolimdromeNumber.js
@@ -1,23 +1,57 @@
-function isPalindrome(num) {
-  if (num < 0) return false;
+/**
+ * leetcode 9
+ * Given an integer x, return true if x is a palindrome , and false otherwise.
 
-  let digits = [];
+Example 1:
+  Input: x = 121
+  Output: true
+  Explanation: 121 reads as 121 from left to right and from right to left.
 
-  while (num >= 10) {
-    digits.push(num % 10);
+Example 2:
+  Input: x = -121
+  Output: false
+  Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+
+Example 3:
+  Input: x = 10
+  Output: false
+  Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+ 
+Constraints:
+  -231 <= x <= 231 - 1
+ 
+Follow up: Could you solve it without converting the integer to a string?
+ */
+
+/**
+ * The Idea behind the algorithm
+ * Just reversing the number and compare in the last if them are equal.
+ * I'm reversing the number by picking the rest of division by ten which is always the last digit of the number, so for ex:
+ *
+ * x = 1245
+ * my while will do:
+ *  first step:
+ *    0 * 10 + 5 => 5;
+ *    num = 124;
+ *  second step:
+ *    5 * 10 + 4 => 54;
+ *    num = 12
+ * and so on.
+ */
+
+function isPalindrome(x) {
+  if (x < 0) return false;
+
+  let num = x;
+  let result = 0;
+
+  while (num > 0) {
+    result = result * 10 + (num % 10);
+
     num = Math.floor(num / 10);
   }
 
-  digits.push(num);
-
-  let left = 0;
-  let right = digits.length - 1;
-
-  while (left <= right) {
-    if (digits[left++] !== digits[right--]) return false;
-  }
-
-  return true;
+  return result === x;
 }
 
 console.log(isPalindrome(121));
