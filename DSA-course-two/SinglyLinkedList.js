10ba3832760dd934b4ce125de9b5943f7dd5e64c
mario-aj <mariojorge1997@mail.ru>
2024-06-03 09:51:25 +0100

    LL: set function

--- DSA-course-two/SinglyLinkedList.js
+++ DSA-course-two/SinglyLinkedList.js
@@ -88,6 +88,15 @@ class SinglyLinkedList {
 
     return current;
   }
+
+  set(index, value) {
+    const node = this.get(index);
+
+    if (!node) return false;
+
+    node.value = value;
+    return true;
+  }
 }
 
 const llist = new SinglyLinkedList();
