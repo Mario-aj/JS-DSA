83c3f1102eb61038adbc7272b665dfc7e90d3352
mario-aj <mariojorge1997@mail.ru>
2024-06-07 09:06:40 +0100

    findKthFromEnd: aditional infos

--- leetcode/LinkedList/findKthFromEnd.js
+++ leetcode/LinkedList/findKthFromEnd.js
@@ -1,6 +1,6 @@
 /**
 LL: Find Kth Node From End ( ** Interview Question)
-Implement a member function called findKthFromEnd(k) that finds and returns the kth node from the end of the linked list.
+Implement a member function called findKthFromEnd(head, k) that finds and returns the kth node from the end of the linked list.
 
 Note: this LinkedList implementation does not have a length member variable.
 
@@ -14,23 +14,23 @@ Constraints:
 
 Example 1:
   Suppose you have a LinkedList object, list, with the following values: 1 -> 2 -> 3 -> 4 -> 5
-  After calling the findKthFromEnd(2) function:
-    let kthNode = list.findKthFromEnd(2);
+  After calling the findKthFromEnd(head, 2) function:
+    let kthNode = list.findKthFromEnd(head, 2);
     The kthNode should have the value 4.
 
 Example 2:
   Now suppose you have a LinkedList object, list, with the following values: 1 -> 2 -> 3 -> 4 -> 5 -> 6
-  After calling the findKthFromEnd(4) function:
-    let kthNode = list.findKthFromEnd(4);
+  After calling the findKthFromEnd(head, 4) function:
+    let kthNode = list.findKthFromEnd(head, 4);
     The kthNode should have the value 3.
  */
 
 function findKthFromEnd(head, k) {
-  if (!this.head) return null;
+  if (!head) return null;
 
   let count = 1;
-  let slow = this.head;
-  let fast = this.head;
+  let slow = head;
+  let fast = head;
 
   while (fast) {
     if (count > k) {
