85dabe5446af18857fbd92b46b2810ff3b535940
mario-aj <mariojorge1997@mail.ru>
2024-06-08 12:16:01 +0100

    DLL: new course linked list

---
+++ DSA-course-two/DOublyLinkedList.js
@@ -0,0 +1,66 @@
+class DoublyLInkedListNode {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(value) {
+    const newNode = new DoublyLInkedListNode(value);
+
+    if (!this.head) {
+      this.head = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+    }
+
+    this.tail = newNode;
+    this.length++;
+
+    return this;
+  }
+
+  pop() {
+    if (!this.head) return undefined;
+
+    let temp = this.tail;
+
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = this.tail.prev;
+      this.tail.next = null;
+      temp.prev = null;
+    }
+
+    this.length--;
+
+    return temp;
+  }
+}
+
+const dll = new DoublyLinkedList();
+
+dll.push(1);
+dll.push(2);
+dll.push(3);
+dll.push(4);
+dll.push(5);
+dll.push(5);
+
+console.log(dll);
+
+console.log("======================= POP ========================");
+dll.pop();
+dll.pop();
+console.log(dll);
