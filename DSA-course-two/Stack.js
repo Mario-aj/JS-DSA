33749ee03659fe0a5ef459f2430a92bba6f63b87
mario-aj <mariojorge1997@mail.ru>
2024-06-13 08:59:44 +0100

    Stack: push method

--- DSA-course-two/Stack.js
+++ DSA-course-two/Stack.js
@@ -10,6 +10,24 @@ class Stack {
     this.top = null;
     this.length = 0;
   }
+
+  push(value) {
+    const node = new StackNode(value);
+
+    if (!this.length) {
+      this.top = node;
+    } else {
+      node.next = this.top;
+      this.top = node;
+    }
+
+    this.length++;
+    return this;
+  }
 }
 
 const stack = new Stack();
+
+stack.push(1);
+stack.push(2);
+stack.push(3);
