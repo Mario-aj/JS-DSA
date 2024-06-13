b586f9e93ea1c14eeb1fb0cfca1a92616e8a2ce5
mario-aj <mariojorge1997@mail.ru>
2024-06-13 08:50:40 +0100

    Stack: constructor implamentation

---
+++ DSA-course-two/Stack.js
@@ -0,0 +1,15 @@
+class StackNode {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.top = null;
+    this.length = 0;
+  }
+}
+
+const stack = new Stack();
