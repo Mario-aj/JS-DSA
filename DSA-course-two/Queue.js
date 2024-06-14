6a08b1e3ad0377cd9cd23444805f5fb83bbb6c01
mario-aj <mariojorge1997@mail.ru>
2024-06-14 08:15:50 +0100

    Queue: enqueue method

--- DSA-course-two/Queue.js
+++ DSA-course-two/Queue.js
@@ -11,4 +11,27 @@ class Queue {
     this.last = null;
     this.length = 0;
   }
+
+  enqueue(value) {
+    const newNode = new Node(value);
+
+    if (!this.length) {
+      this.last = newNode;
+      this.fisrt = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = this.last.next;
+    }
+
+    this.length++;
+    return this;
+  }
 }
+
+const queue = new Queue();
+
+console.log("=================== Queue ========================");
+queue.enqueue(1);
+queue.enqueue(2);
+queue.enqueue(3);
+console.log(queue);
