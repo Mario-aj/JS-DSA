a367041af36ffa550e7135989621170f7197f3fd
mario-aj <mariojorge1997@mail.ru>
2024-06-14 08:10:28 +0100

    Queue class implementation

---
+++ DSA-course-two/Queue.js
@@ -0,0 +1,14 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.fisrt = null;
+    this.last = null;
+    this.length = 0;
+  }
+}
