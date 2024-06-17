9e19a8f6fc73a482042a78f0a81d36aa364f0bc0
mario-aj <mariojorge1997@mail.ru>
2024-06-17 09:39:48 +0100

    refactor: isBalancedParentheses

--- leetcode/Stack/balancedParentheses.js
+++ leetcode/Stack/balancedParentheses.js
@@ -1,82 +1,28 @@
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
-  }
-}
+/**
 
-class Stack {
-  constructor() {
-    this.top = null;
-    this.length = 0;
-  }
-
-  push(value) {
-    const newNode = new Node(value);
-
-    if (!this.top) {
-      this.top = newNode;
-    } else {
-      newNode.next = this.top;
-      this.top = newNode;
-    }
-
-    this.length++;
-    return this;
-  }
-
-  pop() {
-    if (!this.top) return false;
-    let temp = this.top;
-    this.top = this.top.next;
-
-    temp.next = null;
-    this.length--;
-    return temp;
-  }
-
-  isEmpty() {
-    return this.top === null;
-  }
-}
+*/
 
 function isBalancedParentheses(parentheses) {
-  const stack = new Stack();
-
-  const isBalanced = (str) => {
-    const temp = stack.pop();
-
-    if (!temp) return false;
-
-    if (temp.value !== str) return false;
-
-    return true;
-  };
+  const stack = [];
 
   for (let parenthese of parentheses) {
-    switch (parenthese) {
-      case "(":
-      case "{":
-      case "[":
-        stack.push(parenthese);
-        break;
+    if (parenthese === "(" || parenthese === "[" || parenthese === "{") {
+      stack.push(parenthese);
+    } else {
+      if (!stack.length) return false;
 
-      case ")": {
-        if (!isBalanced("(")) return false;
-        break;
-      }
-      case "}": {
-        if (!isBalanced("}")) return false;
-        break;
-      }
-      case "]": {
-        if (!isBalanced("]")) return false;
-        break;
-      }
+      let last = stack.pop();
+
+      if (
+        (parenthese === ")" && last !== "(") ||
+        (parenthese === "]" && last !== "[") ||
+        (parenthese === "}" && last !== "{")
+      )
+        return false;
     }
   }
 
-  if (!stack.isEmpty()) return false;
+  if (stack.length) return false;
 
   return true;
 }
