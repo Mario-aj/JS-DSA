a9b5af63ceaaf654af9711b30545ec89ac258dc8
mario-aj <mariojorge1997@mail.ru>
2024-06-17 10:12:02 +0100

    BST: constructor implementation

---
+++ DSA-course-two/BST.js
@@ -0,0 +1,14 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.right = null;
+    this.left = null;
+  }
+}
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+}
+
+const myBST = new BinarySearchTree();
