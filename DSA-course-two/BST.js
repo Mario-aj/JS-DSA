67e5c4ae6d84164bdd547e9c50b9894506f8b2c6
mario-aj <mariojorge1997@mail.ru>
2024-06-18 08:27:52 +0100

    BST: insert method

--- DSA-course-two/BST.js
+++ DSA-course-two/BST.js
@@ -9,6 +9,43 @@ class BinarySearchTree {
   constructor() {
     this.root = null;
   }
+
+  insert(value) {
+    const node = new Node(value);
+
+    if (!this.root) {
+      this.root = node;
+      return;
+    }
+
+    let temp = this.root;
+
+    while (temp) {
+      if (temp.value > value) {
+        if (!temp.left) {
+          temp.left = node;
+          return;
+        }
+
+        temp = temp.left;
+      } else if (temp.value < value) {
+        if (!temp.right) {
+          temp.right = node;
+          return;
+        }
+
+        temp = temp.right;
+      }
+    }
+  }
 }
 
 const myBST = new BinarySearchTree();
+console.log("========================== INSERT ========================");
+myBST.insert(45);
+myBST.insert(50);
+myBST.insert(25);
+console.log(myBST);
+
+myBST.insert(43);
+console.log(myBST);
