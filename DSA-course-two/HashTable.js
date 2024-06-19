ad42b11ded29371fc0021a884feb2476f4d12937
mario-aj <mariojorge1997@mail.ru>
2024-06-19 08:23:58 +0100

    HT: set method implementation

--- DSA-course-two/HashTable.js
+++ DSA-course-two/HashTable.js
@@ -5,15 +5,33 @@ class HashTable {
 
   _hash(key = "") {
     let hash = 0;
-    let BIG_PRIME_NUMBER = 35;
+    let BIG_PRIME_NUMBER = 23;
 
     for (let i = 0; i < key.length; i++) {
       hash =
-        (hahs + key.charCodeAt(i) * BIG_PRIME_NUMBER) % this.dataMap.length;
+        (hash + key.charCodeAt(i) * BIG_PRIME_NUMBER) % this.dataMap.length;
     }
 
     return hash;
   }
+
+  set(key, value) {
+    const index = this._hash(key);
+
+    if (!this.dataMap[index]) {
+      this.dataMap[index] = [];
+    }
+
+    this.dataMap[index].push([key, value]);
+
+    return this;
+  }
 }
 
 const myHash = new HashTable();
+
+myHash.set("lumber", 70);
+myHash.set("washers", 50);
+myHash.set("bolts", 1400);
+
+console.log(myHash.dataMap);
