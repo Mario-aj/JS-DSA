85614028e6ef99a6c08f882f0a7a55f70bd76130
mario-aj <mariojorge1997@mail.ru>
2024-06-21 08:24:36 +0100

    Graph: remove edge between verteces

--- DSA-course-two/Graph.js
+++ DSA-course-two/Graph.js
@@ -17,13 +17,35 @@ class Graph {
     this.adjacencyList[verter1].push(verter2);
     this.adjacencyList[verter2].push(verter1);
   }
+
+  removeEdge(vertex1, vertex2) {
+    if (this.adjacencyList[vertex1] && this.adjacencyList[vertex2]) {
+      this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+        (v) => v !== vertex2
+      );
+
+      this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+        (v) => v !== vertex1
+      );
+
+      return true;
+    }
+
+    return false;
+  }
 }
 
 const graph = new Graph();
 
 graph.addVertex("A");
 graph.addVertex("B");
+graph.addVertex("C");
 console.log(graph);
 
 graph.addEdge("A", "B");
+graph.addEdge("A", "C");
+graph.addEdge("B", "C");
+console.log(graph);
+
+graph.removeEdge("A", "B");
 console.log(graph);
