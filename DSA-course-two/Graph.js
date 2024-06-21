6ec00bc6a8d38ce2133bc33dea9a8b81932173f3
mario-aj <mariojorge1997@mail.ru>
2024-06-21 08:20:06 +0100

    Graph: add edge

--- DSA-course-two/Graph.js
+++ DSA-course-two/Graph.js
@@ -9,9 +9,21 @@ class Graph {
     this.adjacencyList[verter] = [];
     return true;
   }
+
+  addEdge(verter1, verter2) {
+    if (!this.adjacencyList[verter1] || !this.adjacencyList[verter2])
+      return false;
+
+    this.adjacencyList[verter1].push(verter2);
+    this.adjacencyList[verter2].push(verter1);
+  }
 }
 
 const graph = new Graph();
-graph.addVertex("A");
 
+graph.addVertex("A");
+graph.addVertex("B");
+console.log(graph);
+
+graph.addEdge("A", "B");
 console.log(graph);
